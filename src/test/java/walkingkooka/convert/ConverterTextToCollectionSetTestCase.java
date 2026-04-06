0fcac320e650783652deb4bf3269f81d094e3050
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-06 19:02:55 +1000

    ConverterTextToCollectionSet

---
+++ src/test/java/walkingkooka/convert/ConverterTextToCollectionSetTestCase.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.convert;
+
+import walkingkooka.collect.set.ImmutableSet;
+import walkingkooka.collect.set.Sets;
+
+import java.util.Set;
+
+public abstract class ConverterTextToCollectionSetTestCase<S extends ImmutableSet<E>,
+    E,
+    C extends ConverterTextToCollectionSet<S, E, ConverterContext>> extends ConverterTextToCollectionTestCase<S, E, Set<E>, C> {
+
+    ConverterTextToCollectionSetTestCase() {
+        super();
+    }
+
+    @Override //
+    final void convertToCollectionAndCheck(final CharSequence text,
+                                           final ConverterContext context,
+                                           final E... elements) {
+        this.convertToCollectionAndCheck(
+            text,
+            context,
+            Sets.of(elements)
+        );
+    }
+
+    final void convertToCollectionAndCheck(final CharSequence text,
+                                           final ConverterContext context,
+                                           final Set<E> elements) {
+        this.convertAndCheck(
+            this.createConverter(),
+            text,
+            this.collectionType(),
+            context,
+            (S) this.emptySet()
+                .setElements(elements)
+        );
+    }
+
+    abstract S emptySet();
+}
